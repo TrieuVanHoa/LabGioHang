cfc357e03220794af2511c52c2d136629d445bd3
TrieuVanHoa <144805601+TrieuVanHoa@users.noreply.github.com>
2023-10-01 21:13:38 +0700

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,25 @@
+﻿/* Truyền giá trị vào 1  */
+function calculator() {
+    let dg = parseFloat(window.document.getElementById("dg").value);
+    let sl = parseFloat(window.document.getElementById("sl").value);
+    let Ma = parseFloat(window.document.getElementById("Ma").value);
+    window.document.getElementById("tt").innerHTML = dg * sl;
+
+    if (Ma == null) {
+        window.document.getElementById("discount").innerHTML = dg * sl;
+    }
+    else {
+        window.document.getElementById("discount").innerHTML = dg * sl - (dg * sl * Ma / 100);
+    }
+
+}
+
+
+
+/* Truyển giá trị vào 1 thẻ HTML */
+function calculator2() {
+    let dg2 = parseFloat(window.document.getElementById("dg2").value);
+    let sl2 = parseFloat(window.document.getElementById("sl2").value);
+    window.document.getElementById("tt2").innerHTML = dg2 * sl2;
+}
+
